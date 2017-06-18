87dba65eed36a681105998820a6b6940478a75e7
Dmitded <trollske@mail.ru>
2017-06-18 14:45:32 +0300

    Main has been commented on for more understandable use

--- main.cpp
+++ main.cpp
@@ -7,21 +7,26 @@ using namespace std;
 
 int main()
 {
+  //Initialization of variables for working with ciphers
   char exp;
   string str;
   string dict;
   int key;
 
+  //Sequential output of key selection lines
   cout << "Please, chose encrypt/decrypt [e/d]" << endl;
   cin >> exp;
 
+  //A selection branch for use
   switch (exp) {
     case 'e':
     {
+      //Encoding branch
       cout << "Please, chose ceaser/replace [c/r]" << endl;
       cin >> exp;
 
       switch (exp) {
+        //The Caesar coding branch
         case 'c':
         {
           cout << "Enter your string" << endl;
@@ -33,6 +38,7 @@ int main()
           cout <<  billchipher.encrypt(str) << endl;
           break;
         }
+        //The Replace coding branch
         case 'r':
         {
           cout << "Enter your string" << endl;
@@ -48,12 +54,14 @@ int main()
     }
     case 'd':
     {
+      //Decoding branch
       cout << "Please, chose ceaser/replace [c/r]" << endl;
       cin >> exp;
 
       switch (exp) {
       case 'c':
       {
+        //The Caesar coding branch
         cout << "Enter your string" << endl;
         cin >> str;
         cout << "key" << endl;
@@ -65,6 +73,7 @@ int main()
       }
       case 'r':
       {
+        //The Replace coding branch
         cout << "Enter your string" << endl;
         cin >> str;
         cout << "key" << endl;
